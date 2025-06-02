668fcf54fb1d9ae7f5214d2909ea9a01ac5200db
rmsk2 <rmsk2@gmx.de>
2025-06-02 21:34:16 +0200

    Improve tests

--- backend/logic/generators_test.go
+++ backend/logic/generators_test.go
@@ -33,7 +33,7 @@ func TestWeekly(t *testing.T) {
 func TestYearly(t *testing.T) {
 	rem := repo.Reminder{}
 	t1 := time.Date(2025, time.June, 3, 12, 0, 0, 0, time.Local)
-	t2 := time.Date(2025, time.June, 1, 12, 0, 0, 0, time.Local)
+	t2 := time.Date(2025, time.June, 1, 12, 14, 5, 0, time.Local)
 	rem.Spec = t1
 
 	t3 := anniversaryRefTimeGen(&rem, t2)
@@ -41,13 +41,13 @@ func TestYearly(t *testing.T) {
 		t.Errorf("Test Y1 failed: %v", t3)
 	}
 
-	t2 = time.Date(2025, time.June, 3, 12, 0, 0, 0, time.Local)
+	t2 = time.Date(2025, time.June, 3, 12, 17, 34, 0, time.Local)
 	t3 = anniversaryRefTimeGen(&rem, t2)
 	if (t3.Year() != t1.Year()+1) || (t3.Month() != t1.Month()) || (t3.Day() != t1.Day()) {
 		t.Errorf("Test Y2 failed")
 	}
 
-	t2 = time.Date(2025, time.June, 4, 12, 0, 0, 0, time.Local)
+	t2 = time.Date(2025, time.June, 4, 12, 8, 25, 0, time.Local)
 	t3 = anniversaryRefTimeGen(&rem, t2)
 	if (t3.Year() != t1.Year()+1) || (t3.Month() != t1.Month()) || (t3.Day() != t1.Day()) {
 		t.Errorf("Test Y3 failed")
