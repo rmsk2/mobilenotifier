7334ae70d2900017f271ba9070fbd8100045aba1
Martin Grap <rmsk2@gmx.de>
2025-06-09 20:07:30 +0200

    Refactor

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -5,17 +5,17 @@ import { monthSelected, newSelected, allSelected } from './globals';
 export default {
   emits: ['select-nav'],
   methods: {
-    emitEvents(id) {
-      switch (id) {
-        case 10:
-          this.$emit('select-nav', monthSelected);
-          break;
-        case 11:
-          this.$emit('select-nav', newSelected);
-          break;
-        default:
-          this.$emit('select-nav', allSelected);
-      }      
+    emitEvent(id) {
+      this.$emit('select-nav', id);
+    },
+    emitNew() {
+      this.emitEvent(newSelected)
+    },
+    emitMonth() {
+      this.emitEvent(monthSelected)
+    },
+    emitAll() {
+      this.emitEvent(allSelected)
     }
   },  
 }
@@ -23,8 +23,8 @@ export default {
 
 <template>
   <div class="navbar"> 
-    <button id="nav_nmonth" class="navbutton" @click="emitEvents(10)">Alle Ereignisse im aktuellen Monat anzeigen</button>
-    <button id="nav_new" class="navbutton" @click="emitEvents(11)">Neues Ereignis anlegen</button>
-    <button id="nav_all" class="navbutton" @click="emitEvents(12)">Alle Ereignisse anzeigen</button>  
+    <button id="nav_nmonth" class="navbutton" @click="emitMonth">Alle Ereignisse im aktuellen Monat anzeigen</button>
+    <button id="nav_new" class="navbutton" @click="emitNew">Neues Ereignis anlegen</button>
+    <button id="nav_all" class="navbutton" @click="emitAll">Alle Ereignisse anzeigen</button>  
   </div>
 </template>
