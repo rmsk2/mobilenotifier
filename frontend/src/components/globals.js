6963df8b816bb8315dd70a16d8389fcba798c540
Martin Grap <rmsk2@gmx.de>
2025-06-19 17:42:32 +0200

    Bump version

--- frontend/src/components/globals.js
+++ frontend/src/components/globals.js
@@ -2,7 +2,7 @@ const monthSelected = 1;
 const newSelected = 2;
 const allSelected = 3;
 const aboutSelected = 4;
-const versionString = "0.5.0";
+const versionString = "0.5.1";
 
 export { 
     monthSelected, newSelected, allSelected, aboutSelected, versionString
