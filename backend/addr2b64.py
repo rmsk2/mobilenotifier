b85a701c07d0922298ca41609bf2fbeead0a4f23
Martin Grap <rmsk2@gmx.de>
2025-07-13 16:50:48 +0200

    Improve documentation

--- backend/addr2b64.py
+++ backend/addr2b64.py
@@ -2,6 +2,11 @@ import json
 import sys
 import base64
 
+# This tool is intended to create a more compact version of addr_book.json by removing
+# LF and white space. Additionally the compacted JSON is Base64 encoded as a string
+# without any LFs which in turn can be used to set an environment variable without having 
+# to worry about quoting.
+
 if len(sys.argv) < 2:
     print("usage: addr2b64.py <address_book_file>")
     sys.exit(2)
