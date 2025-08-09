62b273645b53726b269a6e53ba32ff1e9ef530f5
Martin Grap <rmsk2@gmx.de>
2025-08-09 17:55:03 +0200

    Simplify code

--- backend/sms/addressbook.go
+++ backend/sms/addressbook.go
@@ -7,29 +7,24 @@ import (
 )
 
 type AddressSaver struct {
-	recipientMap map[string]repo.Recipient
+	recipients []repo.Recipient
 }
 
 func NewAddressSaverFromJson(jsonData string) (*AddressSaver, error) {
 	var m []repo.Recipient
-	parsedRecipientMap := map[string]repo.Recipient{}
 
 	err := json.Unmarshal([]byte(jsonData), &m)
 	if err != nil {
 		return nil, fmt.Errorf("unable to parse JSON recipient data: %v", err)
 	}
 
-	for _, j := range m {
-		parsedRecipientMap[j.Id.String()] = j
-	}
-
 	return &AddressSaver{
-		recipientMap: parsedRecipientMap,
+		recipients: m,
 	}, nil
 }
 
 func (a *AddressSaver) BBoltSave(db repo.AddrBookWrite) error {
-	for _, v := range a.recipientMap {
+	for _, v := range a.recipients {
 		err := db.Upsert(&v)
 		if err != nil {
 			return fmt.Errorf("error saving address book: %v", err)
